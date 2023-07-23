f558d74a606f75882aecc7a6f94b4e3227e82247
Artur <a.wolniak01@gmail.com>
2023-07-23 16:53:25 +0200

    main.js script for currency converter

---
+++ js/main.js
@@ -0,0 +1,63 @@
+const API_URL = 'https://v6.exchangerate-api.com/v6/72b705c9060d09cbcebf52e7/latest/PLN'
+const selectFrom = document.getElementById('from')
+const selectTo = document.getElementById('to')
+const submitBtn = document.querySelector('#submit_button')
+const swapBtn = document.querySelector('#swap_button')
+
+// function currencies() {
+// 	fetch(currencies_URL)
+// 		.then(res => res.json())
+// 		.then(data => {
+// 			// const option = document.createElement('option')
+// 			// option.value = data.conversion_rates
+// 			// console.log(option)
+// 			console.log(data.supported_codes)
+// 		})
+// 		.catch(err => console.log(err))
+// }
+
+currencies.forEach(currency => {
+	const option = document.createElement('option')
+	option.value = currency
+	option.text = currency
+	selectFrom.add(option)
+})
+
+currencies.forEach(currency => {
+	const option = document.createElement('option')
+	option.value = currency
+	option.text = currency
+	selectTo.add(option)
+})
+
+selectFrom.value = 'PLN'
+
+let swap = () => {
+	let temporaryValue = selectTo.value
+	selectTo.value = selectFrom.value
+	selectFrom.value = temporaryValue
+}
+
+let convert = () => {
+	const amount = document.getElementById('amount').value
+	const fromValue = selectFrom.value
+	const toValue = selectTo.value
+
+	if (amount.length != 0) {
+		fetch(API_URL)
+			.then(res => res.json())
+			.then(data => {
+				let fromExchangeRate = data.conversion_rates[fromValue]
+				let toExchangeRate = data.conversion_rates[toValue]
+				const convertedAmount = (amount / fromExchangeRate) * toExchangeRate
+				result.innerHTML = `${amount} ${fromValue} = ${convertedAmount.toFixed(2)} ${toValue}`
+			})
+	} else {
+		result.innerHTML = 'Please fill the input value'
+	}
+}
+
+console.log(submitBtn)
+submitBtn.addEventListener('click', convert)
+swapBtn.addEventListener('click', swap)
+window.addEventListener('load', convert)
